e7124d7a4c5169e15f0fbd93410bcda295b91010
Romain Giovanetti <ganoninc@gmail.com>
2019-12-26 16:41:03 +0100

    add photos folder mapping method

--- buildPhotoLibraryRessources.py
+++ buildPhotoLibraryRessources.py
@@ -5,19 +5,52 @@
 Those ressources include optimized images, thumbnails and, properties files.
 """
 
+import os
+import glob
 import json
 
 PACKAGE_FILE_PATH = './package.json'
 PHOTOS_FOLDER_PATH = './photos/'
+SUPPORTED_PHOTO_TYPES = ['.jpg']
 PHOTO_LIBRARY_RESSOURCES_FOLDER_PATH = './public/photo-library-ressources/'
 
 def load_package_file():
     with open(PACKAGE_FILE_PATH) as json_file:
         return json.load(json_file)
 
+
+def build_photos_folder_mapping():
+    photos_folder_mapping = {'all_photos': [], 'albums': {}}
+    photo_id = 0
+    photos_grabbed = []
+
+    for supported_photo_type in SUPPORTED_PHOTO_TYPES:
+        pathname = PHOTOS_FOLDER_PATH + '**/*' + supported_photo_type
+        photos_grabbed.extend(
+            glob.glob(pathname, recursive=True))
+
+    for photo_path in photos_grabbed:
+        photo_folder_tree_element = {'id': photo_id, 'path': photo_path}
+        photos_folder_mapping['all_photos'].append(photo_folder_tree_element)
+
+        splitted_photo_path_in_photos_folder_path = photo_path[len(
+            PHOTOS_FOLDER_PATH):len(photo_path)].split('/')
+        if len(splitted_photo_path_in_photos_folder_path) == 2:
+            album_name = splitted_photo_path_in_photos_folder_path[0]
+            if album_name not in photos_folder_mapping['albums']:
+                photos_folder_mapping['albums'][album_name] = {'photos': []}
+            photos_folder_mapping['albums'][album_name]['photos'].append(
+                photo_folder_tree_element)
+        photo_id += 1
+
+    return photos_folder_mapping
+
+
 def build_photo_library_ressources():
     package_file = load_package_file()
     flowtos_version = package_file['version']
+    print("Building photo library ressources for Flowtos version: ", flowtos_version)
+    photos_folder_mapping = build_photos_folder_mapping()
 
 if __name__ == '__main__':
     build_photo_library_ressources()
