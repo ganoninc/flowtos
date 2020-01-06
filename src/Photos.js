a622a03c64582c0b8f62b6ce11be0413f88835b6
Romain Giovanetti <ganoninc@gmail.com>
2020-01-06 21:15:19 +0100

    add lightbox

--- src/Photos.js
+++ src/Photos.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import Gallery from "react-photo-gallery";
+import FsLightbox from 'fslightbox-react'; 
+
 
 function Photos(props) {
-    console.log(props);
-    const photos = props.list.map(photo => { return { src: photo.thumbnailUrl, width: photo.width, height: photo.height} });
-    console.log(photos);
-    return <Gallery photos={photos} />;
+    // remapping of the photo list
+    const photoThumbnails = props.list.map(photo => { return { src: photo.thumbnailUrl, width: photo.width, height: photo.height, key: photo.id.toString()} });
+    const photos = props.list.map(photo => photo.photoUrl);
+
+    const [lightboxController, setLightboxController] = useState({
+        toggler: false,
+        slide: null
+    }); 
+
+    const openLightboxOnSlide = useCallback((event, { photo, index }) => { 
+        setLightboxController({
+            toggler: !lightboxController.toggler,
+            slide: index + 1
+        });
+    }, [lightboxController.toggler]);
+    
+    return (
+        <>
+            <Gallery photos={photoThumbnails} onClick={openLightboxOnSlide}/>
+            <FsLightbox
+                toggler={lightboxController.toggler}
+                sources={photos}
+                slide={lightboxController.slide}
+            /> 
+        </>
+    );
 }
 
 export default Photos;
