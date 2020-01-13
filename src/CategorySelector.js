7843afd53370c3973660b07419b921454d0f98ff
Romain Giovanetti <ganoninc@gmail.com>
2020-01-13 18:10:22 +0100

    make sure the page won't reload when a category is selected

--- src/CategorySelector.js
+++ src/CategorySelector.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav'
+import {
+    useHistory
+} from "react-router-dom";
 
 function CategorySelector(props) {
+    let history = useHistory();
+
+    let onSelectHandler = (selectedKey) => {
+        if (selectedKey === "latest-photos")
+            history.push("/");
+        else if (selectedKey === "albums")
+            history.push("/albums");
+    };
+
     return (
         <>
-            <Nav className="justify-content-center pt-2" activeKey={props.selected}>
+            <Nav className="justify-content-center pt-2" activeKey={props.selected} onSelect={onSelectHandler}>
                 <Nav.Item>
-                    <Nav.Link href="/" eventKey="latest-photos">Latest Photos</Nav.Link>
+                    <Nav.Link eventKey="latest-photos">Latest Photos</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link href="/albums" eventKey="albums">Albums</Nav.Link>
+                    <Nav.Link eventKey="albums">Albums</Nav.Link>
                 </Nav.Item>
             </Nav>
         </>
