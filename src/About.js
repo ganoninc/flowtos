6a413443e320dd69df02d65951183e53af804e9b
Romain Giovanetti <ganoninc@gmail.com>
2020-01-15 20:33:19 +0100

    add about page + about me section

---
+++ src/About.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function About(props) {
+  return (
+    <div className="about">
+      <h2 className="pb-3">Me</h2>
+      <p>
+        <i>Bonjour</i>, I'm Romain.
+      </p>
+      <p>
+        As far as I remember, I've always loved taking pictures. I started my
+        jourmey with the camera of my father, an old one with a very limited
+        photographic film, with which each picture was precious. Then I won a
+        compact ditial camera when I was 12 years old that eventually died when
+        I was 17. A few years after the iPhone arrived in my life and changed
+        everything. Eventually, one year ago I bought a professional digital
+        camera and just 2 minutes after the transaction, I got hit by a car as
+        pedestrian, but that's another story.{" "}
+      </p>
+      <p>
+        Long story short, I've enjoyed taking and editing those pictures and I
+        hope you'll like them.
+      </p>
+    </div>
+  );
+}
+
+export default About;
--- src/Flowtos.js
+++ src/Flowtos.js
@@ -1,124 +1,148 @@
-import React from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route
-} from "react-router-dom";
-import LoadingIndicator from './LoadingIndicator'
-import Photos from './Photos';
-import Albums from './Albums';
-import Album from './Album';
-import CategorySelector from './CategorySelector'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoadingIndicator from "./LoadingIndicator";
+import Photos from "./Photos";
+import Albums from "./Albums";
+import Album from "./Album";
+import About from "./About";
+import CategorySelector from "./CategorySelector";
 
-import './Flowtos.scss';
+import "./Flowtos.scss";
 
 class Flowtos extends React.Component {
+  PHOTO_LIBRARY_ENDPOINT =
+    "https://giovanetti.fr/flowtos/photo-library-ressources/";
 
-    PHOTO_LIBRARY_ENDPOINT = "https://giovanetti.fr/flowtos/photo-library-ressources/";
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.state = {
+      error: null,
+      isPhotoLibraryIndexLoaded: false,
+      allPhotosIndex: []
+    };
 
-        this.state = {
-            error: null,
-            isPhotoLibraryIndexLoaded: false,
-            allPhotosIndex: []
-        };
+    if (process.env.NODE_ENV !== "production") {
+      this.PHOTO_LIBRARY_ENDPOINT =
+        "http://localhost:3000/photo-library-ressources/";
+    }
+  }
 
-        if (process.env.NODE_ENV !== 'production') {
-            this.PHOTO_LIBRARY_ENDPOINT = "http://localhost:3000/photo-library-ressources/";
+  componentDidMount() {
+    fetch(this.PHOTO_LIBRARY_ENDPOINT + "index.json")
+      .then(res => res.json())
+      .then(
+        photoLibraryIndex => {
+          this.setState({
+            isPhotoLibraryIndexLoaded: true,
+            allPhotosIndex: photoLibraryIndex.all_photos,
+            albumsIndex: photoLibraryIndex.albums
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
         }
+      );
+  }
+
+  render() {
+    let photosView;
+    const {
+      error,
+      isPhotoLibraryIndexLoaded,
+      allPhotosIndex,
+      albumsIndex
+    } = this.state;
+
+    if (error) {
+      photosView = <div>Error while loading Flowtos: {error.message}</div>;
+    } else if (isPhotoLibraryIndexLoaded) {
+      photosView = (
+        <>
+          <Switch>
+            <Route path="/photos/:photoId">
+              <Photos
+                photoList={allPhotosIndex}
+                photoLibraryEndpoint={this.PHOTO_LIBRARY_ENDPOINT}
+              />
+            </Route>
+            <Route exact path="/albums/:albumId">
+              <Album
+                albumList={albumsIndex}
+                photoLibraryEndpoint={this.PHOTO_LIBRARY_ENDPOINT}
+              />
+            </Route>
+            <Route exact path="/albums/:albumId/:photoId">
+              <Album
+                albumList={albumsIndex}
+                photoLibraryEndpoint={this.PHOTO_LIBRARY_ENDPOINT}
+              />
+            </Route>
+            <Route path="/albums">
+              <Albums
+                albumList={albumsIndex}
+                photoLibraryEndpoint={this.PHOTO_LIBRARY_ENDPOINT}
+              />
+            </Route>
+            <Route path="/about">
+              <About photoLibraryEndpoint={this.PHOTO_LIBRARY_ENDPOINT} />
+            </Route>
+            <Route path="/">
+              <Photos
+                photoList={allPhotosIndex}
+                photoLibraryEndpoint={this.PHOTO_LIBRARY_ENDPOINT}
+              />
+            </Route>
+          </Switch>
+        </>
+      );
+    } else {
+      photosView = <LoadingIndicator />;
     }
 
-    componentDidMount() {
-        fetch(this.PHOTO_LIBRARY_ENDPOINT + "index.json")
-            .then(res => res.json())
-            .then(
-                (photoLibraryIndex) => {
-                    this.setState({
-                        isPhotoLibraryIndexLoaded: true,
-                        allPhotosIndex: photoLibraryIndex.all_photos,
-                        albumsIndex: photoLibraryIndex.albums
-
-                    });
-                },
-                // Note: it's important to handle errors here
-                // instead of a catch() block so that we don't swallow
-                // exceptions from actual bugs in components.
-                (error) => {
-                    this.setState({
-                        isLoaded: true,
-                        error
-                    });
-                }
-            )
-    }
-
-    render() {
-        let photosView;
-        const { error, isPhotoLibraryIndexLoaded, allPhotosIndex, albumsIndex } = this.state;
-
-        if (error) {
-            photosView = <div>Error while loading Flowtos: {error.message}</div>;
-        }
-        else if (isPhotoLibraryIndexLoaded) {
-            photosView = (
-                <>
-                <Switch>
-                    <Route path="/photos/:photoId">
-                        <Photos photoList={allPhotosIndex} photoLibraryEndpoint={this.PHOTO_LIBRARY_ENDPOINT} />
-                    </Route>
-                    <Route exact path="/albums/:albumId">
-                        <Album albumList={albumsIndex} photoLibraryEndpoint={this.PHOTO_LIBRARY_ENDPOINT} />
-                    </Route>
-                    <Route exact path="/albums/:albumId/:photoId">
-                        <Album albumList={albumsIndex} photoLibraryEndpoint={this.PHOTO_LIBRARY_ENDPOINT} />
-                    </Route>
-                    <Route path="/albums">
-                        <Albums albumList={albumsIndex} photoLibraryEndpoint={this.PHOTO_LIBRARY_ENDPOINT} />
-                    </Route>
-                    <Route path="/">
-                        <Photos photoList={allPhotosIndex} photoLibraryEndpoint={this.PHOTO_LIBRARY_ENDPOINT} />
-                    </Route>
-                </Switch>
-                </>
-            );
-        } else {
-            photosView = <LoadingIndicator />;
-        }
-
-        return (
-            <div className="flowtos">
-                <Router>
-                    <div className="container">
-                        <header className="Flowtos-header pt-5 pb-4">
-                            <span className="text-muted">Romain J. Giovanetti</span>
-                            <h1 className="pt-2">Jeu de Lumières</h1>
-                            <Switch>
-                                <Route path="/photos/:photoId">
-                                    <CategorySelector selected="latest-photos" />
-                                </Route>
-                                <Route path="/albums">
-                                    <CategorySelector selected="albums" />
-                                </Route>
-                                <Route path="/">
-                                    <CategorySelector selected="latest-photos" />
-                                </Route>
-                            </Switch>
-                        </header>
-                        <main>
-                            {photosView}
-                        </main>
-                        <footer className="footer mt-auto py-4">
-                            <div className="container">
-                                <span className="text-muted">All images © 2002-2020 <a href="https://www.giovanetti.fr/">Romain J. Giovanetti</a> (@Gioroju). </span>
-                            </div>
-                        </footer>
-                    </div>
-                </Router>
-            </div>
-        );
-    }
+    return (
+      <div className="flowtos">
+        <Router>
+          <div className="container">
+            <header className="Flowtos-header pt-5 pb-4">
+              <span className="text-muted">Romain J. Giovanetti</span>
+              <h1 className="pt-2">Jeu de Lumières</h1>
+              <Switch>
+                <Route path="/photos/:photoId">
+                  <CategorySelector selected="latest-photos" />
+                </Route>
+                <Route path="/albums">
+                  <CategorySelector selected="albums" />
+                </Route>
+                <Route path="/about">
+                  <CategorySelector selected="about" />
+                </Route>
+                <Route path="/">
+                  <CategorySelector selected="latest-photos" />
+                </Route>
+              </Switch>
+            </header>
+            <main>{photosView}</main>
+            <footer className="footer mt-auto py-4">
+              <div className="container">
+                <span className="text-muted">
+                  All images © 2002-2020{" "}
+                  <a href="https://www.giovanetti.fr/">Romain J. Giovanetti</a>{" "}
+                  (@Gioroju).{" "}
+                </span>
+              </div>
+            </footer>
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default Flowtos;
