2e1688b96c7005203448bb73e83cdfe4947ea161
Romain Giovanetti <ganoninc@gmail.com>
2020-01-16 07:09:02 +0100

    improve About content

--- src/About.js
+++ src/About.js
@@ -9,13 +9,15 @@ function About(props) {
       </p>
       <p>
         As far as I remember, I've always loved taking pictures. I started my
-        jourmey with the camera of my father, an old one with a very limited
-        photographic film, with which each picture was precious. Then I won a
-        compact ditial camera when I was 12 years old that eventually died when
+        journey with the camera of my father, an old one with a very limited
+        photographic film, with which each picture was precious. Then, I won a
+        compact digital camera when I was 12 years old that eventually died when
         I was 17. A few years after the iPhone arrived in my life and changed
-        everything. Eventually, one year ago I bought a professional digital
-        camera and just 2 minutes after the transaction, I got hit by a car as
-        pedestrian, but that's another story.
+        everything because I was now able to carry a camera all the time, right
+        in my pocket, always ready to shoot the sometimes unpredictable.
+        Eventually, in December 19', I bought a professional digital camera to
+        go beyond the limitations of my smartphone and just 2 minutes after the
+        transaction, I got hit by a car as pedestrian, but that's another story.
       </p>
       <p>
         Long story short, I've enjoyed taking and editing those pictures and I
