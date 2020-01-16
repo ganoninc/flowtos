e2cc9ddfd3606b693f6bd0d00f2d8ebc0d97dd61
Romain Giovanetti <ganoninc@gmail.com>
2020-01-16 07:10:15 +0100

    switch pictures to photos for consistency

--- src/About.js
+++ src/About.js
@@ -8,9 +8,9 @@ function About(props) {
         <i>Bonjour</i>, I'm Romain.
       </p>
       <p>
-        As far as I remember, I've always loved taking pictures. I started my
+        As far as I remember, I've always loved taking photos. I started my
         journey with the camera of my father, an old one with a very limited
-        photographic film, with which each picture was precious. Then, I won a
+        photographic film, with which each photo was precious. Then, I won a
         compact digital camera when I was 12 years old that eventually died when
         I was 17. A few years after the iPhone arrived in my life and changed
         everything because I was now able to carry a camera all the time, right
@@ -20,7 +20,7 @@ function About(props) {
         transaction, I got hit by a car as pedestrian, but that's another story.
       </p>
       <p>
-        Long story short, I've enjoyed taking and editing those pictures and I
+        Long story short, I've enjoyed taking and editing those photos and I
         hope you'll like them.
       </p>
     </div>
