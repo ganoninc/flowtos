15636ad938026178a50e61eb8f9bc5a82c10371c
Romain Giovanetti <ganoninc@gmail.com>
2020-01-18 12:11:00 +0100

    refactor code of Albums

--- src/Albums.js
+++ src/Albums.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import AlbumCard from './AlbumCard'
+import React from "react";
+import AlbumCard from "./AlbumCard";
 
 function Albums(props) {
-    let albumList = props.albumList.map(album => {
-        let coverImg = props.photoLibraryEndpoint + album.photos[0].thumbnailUrl;
-        let coverImg2x = props.photoLibraryEndpoint + album.photos[0].thumbnail2xUrl;
-        
-        return (
-            <AlbumCard key={album.name} cover={coverImg} cover2x={coverImg2x} name={album.name}/>
-        );
-    });
+  const { albumList, photoLibraryEndpoint } = props;
+
+  let albumListComponents = albumList.map(album => {
+    let coverImg = photoLibraryEndpoint + album.photos[0].thumbnailUrl;
+    let coverImg2x = photoLibraryEndpoint + album.photos[0].thumbnail2xUrl;
 
     return (
-        <div className="row justify-content-center">
-            {albumList}
-        </div>
+      <AlbumCard
+        key={album.name}
+        cover={coverImg}
+        cover2x={coverImg2x}
+        name={album.name}
+      />
     );
+  });
+
+  return (
+    <div className="row justify-content-center">{albumListComponents}</div>
+  );
 }
 
 export default Albums;
