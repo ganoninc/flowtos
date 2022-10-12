efb82882cdbb1e7df75b3ff221591157b501df4b
Romain Giovanetti <ganoninc@gmail.com>
2022-10-12 11:21:06 +1100

    fix ignored jpg files

--- bplrh_helpers/extra/macos_photos_export_adapter.py
+++ bplrh_helpers/extra/macos_photos_export_adapter.py
@@ -14,6 +14,9 @@ import time
 def update_folder(working_directory):
     if os.path.isdir(working_directory):
         files = glob.glob(working_directory + '/*.jpeg', recursive=False)
+        files.extend(glob.glob(working_directory + '/*.JPEG', recursive=False))
+        files.extend(glob.glob(working_directory + '/*.jpg', recursive=False))
+        files.extend(glob.glob(working_directory + '/*.JPG', recursive=False))
         files.sort(key=os.path.getmtime, reverse=True)
         for file in files:
             Path(file).touch()
